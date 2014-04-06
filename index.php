27406dcc17d6911559cb46f214272c988bef5ef9
Jiri <j.pavelka@seznam.cz>
2014-04-06 23:07:22 +0200

    Config s demonstraci

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 # Global constants
 
-define('SOURCE_FOLDER', 'src');
-
+define('SOURCE_FOLDER', 'src'); // where objects and other src are stored
+define('ADMIN_FOLDER', 'adm'); // where admin cfg xml files are stored
+define('USER_FOLDER', 'usr'); // where user cfg xml files are stored
 
 /**
  * Autoload classes from source folder
--- src/Config.php
+++ src/Config.php
@@ -7,21 +7,51 @@
  */
 class Config {
 
-  function __construct($plugin="default") {
+  const DEBUG = true;
+  private $cfg;
 
+  function __construct($plugin="default") {
     if(!is_string($plugin)) throw new Exception('Variable type: not string.');
 
-    // create DOM from default config xml
-    $cfg = new DOMDocument();
-    if($plugin == "default") $cfg->load("default.xml");
-    else $cfg->load("plugins/$plugin/$plugin.xml");
+    $this->cfg = new DOMDocument();
+    $this->cfg->formatOutput = true;
 
-    // modify DOM by specific domain data
-    #TODO
-    
+    // create DOM from default config xml (cms or plugin)
+    if($plugin == "default") $this->cfg->load("default.xml");
+    else $this->cfg->load("plugins/$plugin/$plugin.xml");
+    if($this::DEBUG) echo "<pre>".htmlspecialchars($this->cfg->saveXML())."</pre>";
+
+    // modify DOM by admin data
+    $this->updateDom(ADMIN_FOLDER."/$plugin.xml");
+
+    if($this::DEBUG) echo "<pre>".htmlspecialchars($this->cfg->saveXML())."</pre>";
+
     // modify DOM by user data (except readonly)
-    #TODO
+    $this->updateDom(USER_FOLDER."/$plugin.xml",false);
+    if($this::DEBUG) echo "<pre>".htmlspecialchars($this->cfg->saveXML())."</pre>";
 
+  }
+
+  private function updateDom($xmlFile,$ignoreReadonly=true) {
+    if(!is_string($xmlFile)) throw new Exception('Variable type: not string.');
+
+    $doc = new DOMDocument();
+    $doc->load($xmlFile);
+    $nodes = $doc->firstChild->childNodes;
+    $xPath = new DOMXPath($this->cfg);
+
+    for($i = 0; $i < $nodes->length; $i++) {
+      if($nodes->item($i)->nodeType != 1) continue;
+      $cfgNodes = $xPath->query($nodes->item($i)->getNodePath());
+      // only elements pass
+      if($cfgNodes->length != 1) continue;
+      // only without attribute readonly
+      if(!$ignoreReadonly
+        && $cfgNodes->item(0)->getAttribute("readonly") == "readonly") continue;
+      $this->cfg->firstChild->replaceChild(
+        $this->cfg->importNode($nodes->item($i),true),$cfgNodes->item(0)
+      );
+    }
   }
 
 }
