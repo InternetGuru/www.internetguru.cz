dc2291881585ba3465f3303f02a06c8cd7a97caf
Pavel Petrzela <pavel@petrzela.eu>
2014-05-08 13:44:58 +0200

    reapir define global constatnts (add full path); repair dombuilder name; repair slider plugin folder name

--- index.php
+++ index.php
@@ -1,53 +1,53 @@
-<?php
+  <?php
 
-#error_reporting(E_ALL);
-#ini_set("display_errors", 1);
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
 
-# Global constants
+  # Global constants
 
-define('SOURCE_FOLDER', 'src'); // where objects and other src are stored
-define('ADMIN_FOLDER', 'adm'); // where admin cfg xml files are stored
-define('USER_FOLDER', 'usr'); // where user cfg xml files are stored
-define('PLUGIN_FOLDER', 'plugins'); // where plugins are stored
+  define('SOURCE_FOLDER', dirname(__FILE__) . '/src'); // where objects and other src are stored
+  define('ADMIN_FOLDER',  dirname(__FILE__) . '/adm'); // where admin cfg xml files are stored
+  define('USER_FOLDER',   dirname(__FILE__) . '/usr'); // where user cfg xml files are stored
+  define('PLUGIN_FOLDER', dirname(__FILE__) . '/plugins'); // where plugins are stored
 
-/**
- * Autoload classes from source folder
- */
-function __autoload($className) {
-  if(is_file(SOURCE_FOLDER . "/$className.php"))
-    include SOURCE_FOLDER . "/$className.php";
-  else
-    include PLUGIN_FOLDER . "/$className/$className.php";
-}
-
-try {
-
-  // init CMS
-  $cms = new Cms();
-
-  // plugin attach into class Plugins (Subject)
-  $plugins = new Plugins();
-  $plugins->setCms($cms);
-  foreach(scandir(PLUGIN_FOLDER) as $plugin) {
-    // omit folders starting with a dot
-    if(substr($plugin,0,1) == ".") continue;
-    $plugins->attach(new $plugin);
+  /**
+   * Autoload classes from source folder
+   */
+  function __autoload($className) {
+    if(is_file(SOURCE_FOLDER . "/$className.php"))
+      include SOURCE_FOLDER . "/$className.php";
+    else
+      include PLUGIN_FOLDER . "/$className/$className.php";
   }
 
-  // notify plugins, status init
-  $plugins->setStatus("init");
-  $plugins->notify();
+  try {
 
-  // notify plugins, status process
-  $plugins->setStatus("process");
-  $plugins->notify();
+    // init CMS
+    $cms = new Cms();
 
-  echo $cms->getOutput();
+    // plugin attach into class Plugins (Subject)
+    $plugins = new Plugins();
+    $plugins->setCms($cms);
+    foreach(scandir(PLUGIN_FOLDER) as $plugin) {
+      // omit folders starting with a dot
+      if(substr($plugin,0,1) == ".") continue;
+      $plugins->attach(new $plugin);
+    }
 
-} catch(Exception $e) {
+    // notify plugins, status init
+    $plugins->setStatus("init");
+    $plugins->notify();
 
-  echo "Exception: ".$e->getMessage();
+    // notify plugins, status process
+    $plugins->setStatus("process");
+    $plugins->notify();
 
-}
+    echo $cms->getOutput();
 
-?>
+  } catch(Exception $e) {
+
+    echo "Exception: ".$e->getMessage();
+
+  }
+
+  ?>
