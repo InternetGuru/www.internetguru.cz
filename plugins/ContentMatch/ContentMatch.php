9600791268482403543b40a96551f87369e81873
Jiri <j.pavelka@seznam.cz>
2014-07-31 21:18:34 +0200

    contentMatch upgrade

--- plugins/ContentMatch/ContentMatch.php
+++ plugins/ContentMatch/ContentMatch.php
@@ -41,40 +41,72 @@ class ContentMatch implements SplObserver, ContentStrategyInterface {
       $this->subject->detach($this);
       return;
     }
-    $link = $this->findSimilar($xpath,$cms->getLink());
+    $link = mb_convert_encoding($cms->getLink(),"ASCII","UTF-8");
+    $links = array();
+    foreach($xpath->query("//h[@link]") as $h) $links[] = $h->getAttribute("link");
+    $linkId = $this->findSimilar($links,$link);
+
+    if($linkId === false) $link = ".";
+    else $link = $links[$linkId];
     $this->redirToLink($link);
   }
 
-  private function findSimilar(DOMXPath $xpath,$link) {
-    $link = mb_convert_encoding($link,"ASCII","UTF-8");
-    $links = array();
-    foreach($xpath->query("//h[@link]") as $h) $links[] = $h->getAttribute("link");
-    // closest substring
-    if(($newLink = $this->similarPos($links,$link)) !== false) return $newLink;
-    // max typo
-    if(($newLink = $this->bestLeven($links,$link,3)) !== false) return $newLink;
-    // else go to homepage
-    return ".";
+  /**
+   * exists: aa/bb/cc/dd, aa/bb/cc/ee, aa/bb/dd, aa/dd
+   * call: aa/b/cc/dd -> find aa/bb/cc/dd (not aa/dd)
+   */
+  private function findSimilar(Array $links,$link) {
+    if(!strlen($link)) return false;
+    // zero pos substring
+    if(($newLink = $this->minPos($links,$link,0)) !== false) return $newLink;
+    // low levenstein first
+    if(($newLink = $this->minLev($links,$link,1)) !== false) return $newLink;
+
+    $parts = explode("/", $link);
+    $first = array_shift($parts);
+    $subset = array();
+    foreach($links as $k => $l) {
+      if(strpos($l,$first) !== 0) continue;
+      if(strpos($l,"/") === false) continue;
+      else $subset[$k] = substr($l,strpos($l,"/")+1);
+    }
+    if(count($subset) == 1) return key($subset);
+    if(empty($subset)) $subset = $links;
+    return $this->findSimilar($subset,implode("/",$parts));
   }
 
-  private function similarPos(Array $links,$link) {
+  private function minPos(Array $links,$link,$max) {
     $linkpos = array();
-    foreach ($links as $l) {
+    foreach ($links as $k => $l) {
       $pos = strpos($l, $link);
-      if($pos === false) continue;
-      $linkpos[$l] = $pos;
+      if($pos === false || $pos > $max) continue;
+      $linkpos[$k] = $pos;
     }
     asort($linkpos);
     if(!empty($linkpos)) return key($linkpos);
-    return false;
+    $sublinks = array();
+    foreach($links as $k => $l) {
+      $l = str_replace(array("_","-"), "/", $l);
+      if(strpos($l,"/") === false) continue;
+      $sublinks[$k] = substr($l,strpos($l,"/")+1);
+    }
+    if(empty($sublinks)) return false;
+    return $this->minPos($sublinks,$link,$max);
   }
 
-  private function bestLeven(Array $links,$link,$limit) {
+  private function minLev(Array $links,$link,$limit) {
     $leven = array();
-    foreach ($links as $l) $leven[$l] = levenshtein($l, $link);
+    foreach ($links as $k => $l) $leven[$k] = levenshtein($l, $link);
     asort($leven);
     if(reset($leven) <= $limit) return key($leven);
-    return false;
+    $sublinks = array();
+    foreach($links as $k => $l) {
+      $l = str_replace(array("_","-"), "/", $l);
+      if(strpos($l,"/") === false) continue;
+      $sublinks[$k] = substr($l,strpos($l,"/")+1);
+    }
+    if(empty($sublinks)) return false;
+    return $this->minLev($sublinks,$link,$limit);
   }
 
   private function redirToLink($link) {
