5d49439edb7e304ba3627dd18fd93564765be67e
Jiri <j.pavelka@seznam.cz>
2014-08-27 19:55:18 +0200

    ContentAdmin file logic

--- plugins/ContentAdmin/ContentAdmin.php
+++ plugins/ContentAdmin/ContentAdmin.php
@@ -14,7 +14,6 @@ class ContentAdmin extends Plugin implements SplObserver, ContentStrategyInterfa
   private $content = null;
   private $errors = array();
   private $contentValue = "";
-  private $destinationFile = null;
   private $schema = null;
   private $adminLink;
   private $type;
@@ -31,6 +30,8 @@ class ContentAdmin extends Plugin implements SplObserver, ContentStrategyInterfa
     $this->subject = $subject;
     $this->adminLink = $subject->getCms()->getLink()."?admin";
     try {
+
+      $this->setDefaultFile();
       $this->processAdmin();
     } catch (Exception $e) {
       $this->errors[] = $e->getMessage();
@@ -46,21 +47,20 @@ class ContentAdmin extends Plugin implements SplObserver, ContentStrategyInterfa
     $format = $this->type;
     if(!is_null($this->schema)) $format .= " ({$this->schema})";
 
-    $lad = $this->adminLink ."=". $this->defaultFile;
-    $la = $this->adminLink ."=". $this->destinationFile;
+    $la = $this->adminLink ."=". $this->defaultFile;
+    $usrDestHash = $this->getFileHash(USER_FOLDER ."/". $this->defaultFile)."";
 
     $newContent = $this->getHTMLPlus();
     $newContent->insertVar("heading",$cms->getTitle(),"ContentAdmin");
     $newContent->insertVar("errors",$this->errors,"ContentAdmin");
     $newContent->insertVar("link",$cms->getLink(),"ContentAdmin");
-    $newContent->insertVar("linkAdminDefault",$lad,"ContentAdmin");
     $newContent->insertVar("linkAdmin",$la,"ContentAdmin");
     $newContent->insertVar("content",$this->contentValue,"ContentAdmin");
-    $newContent->insertVar("filename",$this->destinationFile,"ContentAdmin");
+    $newContent->insertVar("filename",$this->defaultFile,"ContentAdmin");
     $newContent->insertVar("schema",$format,"ContentAdmin");
     $newContent->insertVar("mode",$this->type,"ContentAdmin");
     #$newContent->insertVar("noparse","noparse","ContentAdmin");
-    $newContent->insertVar("filehash",$this->getFileHash($this->destinationFile)."","ContentAdmin");
+    $newContent->insertVar("userfilehash",$usrDestHash,"ContentAdmin");
 
     return $newContent;
   }
@@ -73,87 +73,109 @@ class ContentAdmin extends Plugin implements SplObserver, ContentStrategyInterfa
     return hash_file(self::HASH_ALGO,$filePath);
   }
 
-  private function processAdmin() {
+  /**
+   * LOGIC (-> == redir if exists)
+   * F -> plugins/$0/$0.xml -> $0.xml
+   * [dir/]+F -> $0.xml
+   * [dir/]*F.ext (direct match) -> plugins/$0
+   *
+   * EXAMPLES
+   * Xhtml11 -> plugins/Xhtml11/Xhtml11.xml (F default plugin config)
+   * Xhtml11/Xhtml11.xsl -> plugins/Xhtml11/Xhtml11.xsl (dir/F.ext plugin)
+   * Cms.xml -> Cms.xml (F.ext direct match)
+   * themes/SimpleLayout.css -> themes/SimpleLayout.css (dir/F.ext direct)
+   * themes/userFile.css -> usr/themes/userFile.css (dir/F.ext user)
+   */
+  private function setDefaultFile() {
 
-    $f = USER_FOLDER . "/" . self::DEFAULT_FILE;
+    $f = self::DEFAULT_FILE;
     if(strlen($_GET["admin"])) $f = $_GET["admin"];
     if(strpos($f,"/") === 0) $f = substr($f,1); // remove trailing slash
-    $usrFile = false;
-    $this->defaultFile = $f;
+
+    // direct user/admin file input is disabled
     if(strpos($f,USER_FOLDER."/") === 0) {
-      $usrFile = true;
-      $this->defaultFile = substr($f,strlen(USER_FOLDER)+1);
+      $this->redir(substr($f,strlen(USER_FOLDER)+1));
     }
     if(strpos($f,ADMIN_FOLDER."/") === 0) {
-      throw new Exception("Cannot edit admin files");
+      $this->redir(substr($f,strlen(ADMIN_FOLDER)+1));
     }
-    $this->destinationFile = USER_FOLDER."/".$this->defaultFile;
-    $this->type = pathinfo($this->defaultFile,PATHINFO_EXTENSION);
+
+    $this->defaultFile = $f;
+
+    // no extension
+    $this->type = pathinfo($f,PATHINFO_EXTENSION);
     if($this->type == "") {
-      $pluginFile = PLUGIN_FOLDER."/{$this->defaultFile}/{$this->defaultFile}.xml";
-      if(!findFile($pluginFile)) $this->redir();
-      $this->redir(USER_FOLDER."/$pluginFile");
+      $pluginFile = PLUGIN_FOLDER."/$f/$f.xml";
+      if(!findFile($pluginFile)) $this->redir("$f.xml");
+      $this->redir($pluginFile);
     }
-    $df = findFile($this->defaultFile,false,false);
+
+    // no direct match with extension [and path]
+    if(!findFile($f,false)) {
+      // check/redir to plugin dir
+      if(findFile(PLUGIN_FOLDER . "/$f",false))
+        $this->redir(PLUGIN_FOLDER . "/$f");
+      // file not found. create user file?
+      throw new Exception("File '$f' not found. Cannot create user file.");
+    }
+  }
+
+  private function processXml($post) {
+    if(!in_array($this->type,array("xml","xsl"))) return;
+
+    // get default schema
+    if($df = findFile($this->defaultFile,false)) {
+      $this->schema = $this->getSchema($df);
+    }
+    // get user schema if default schema not exists
+    if(is_null($this->schema) && file_exists(USER_FOLDER ."/". $this->defaultFile)) {
+      $this->schema = $this->getSchema(USER_FOLDER ."/". $this->defaultFile);
+    }
+
+    if($post) {
+      if($this->isHtmlPlus()) $doc = new HTMLPlus();
+      else $doc = new DOMDocumentPlus();
+      if(!@$doc->loadXml($this->contentValue))
+        throw new Exception("Invalid XML syntax");
+    } else $doc = $this->loadXml(USER_FOLDER ."/". $this->defaultFile);
+
+    $doc->formatOutput = true;
+    if($this->isHtmlPlus()) {
+      $doc->validatePlus(true);
+      if($doc->isAutocorrected()) $this->contentValue = $doc->saveXML();
+    } else {
+      $doc->validatePlus();
+      if($df && $doc->removeNodes("//*[@readonly]"))
+        $this->contentValue = $doc->saveXML();
+      $this->validateXml($doc);
+    }
+  }
+
+  private function processAdmin() {
 
     $post = false;
-    if(isset($_POST["content"],$_POST["filehash"])) {
+    if(isset($_POST["content"],$_POST["userfilehash"])) {
       $post = true;
       $post_n = str_replace("\r\n", "\n", $_POST["content"]);
       $post_rn = str_replace("\n", "\r\n", $post_n);
       $this->contentValue = $post_n;
-      if(in_array($_POST["filehash"],array($this->getHash($post_n),$this->getHash($post_rn))))
+      if(in_array($_POST["userfilehash"],array($this->getHash($post_n),$this->getHash($post_rn))))
         throw new Exception("No changes made");
-      if($_POST["filehash"] != $this->getFileHash($this->destinationFile))
-        throw new Exception("User file '{$this->destinationFile}' has changed during administration");
+      if($_POST["userfilehash"] != $this->getFileHash(USER_FOLDER ."/". $this->defaultFile))
+        throw new Exception("User file '{$this->defaultFile}' has changed during administration");
     } else {
-      $this->contentValue = $this->loadFile($f,$df);
+      $this->contentValue = $this->loadFile(USER_FOLDER . "/" . $this->defaultFile);
     }
 
-    if(in_array($this->type,array("xml","xsl"))) {
-      if($df) {
-        $this->schema = $this->getSchema($df);
-      }
-      if(is_null($this->schema) && file_exists($f)) {
-        $this->schema = $this->getSchema($f);
-      }
-      if($post) {
-        if($this->isHtmlPlus()) $doc = new HTMLPlus();
-        else $doc = new DOMDocumentPlus();
-        if(!@$doc->loadXml($this->contentValue))
-          throw new Exception("Invalid XML syntax");
-      } else $doc = $this->loadXml($f,$df);
-      $doc->formatOutput = true;
-      if($this->isHtmlPlus()) {
-        $doc->validatePlus(true);
-        if($doc->isAutocorrected()) $this->contentValue = $doc->saveXML();
-      } else {
-        $doc->validatePlus();
-        if(!$usrFile && $doc->removeNodes("//*[@readonly]"))
-          $this->contentValue = $doc->saveXML();
-        $this->validateXml($doc);
-      }
-    }
+    $this->processXml($post);
 
-    if($post) $this->save();
+    if($post) $this->save(USER_FOLDER ."/". $this->defaultFile, $this->contentValue);
   }
 
-  private function loadFile($f,$def) {
-
-    if(file_exists($f)) {
-      $file = $f;
-      $user = false;
-    }
-    elseif(file_exists($def)) {
-      $file = $def;
-      $user = false;
-    } else {
-      return "";
-    }
-
-    if(!($s = @file_get_contents(findFile($file,$user))))
+  private function loadFile($file) {
+    if(!file_exists($file)) return "";
+    if(!($s = @file_get_contents($file)))
       throw new Exception ("Unable to get contents from '$file'");
-
     return $s;
   }
 
@@ -175,10 +197,10 @@ class ContentAdmin extends Plugin implements SplObserver, ContentStrategyInterfa
     return $db->buildDOMPlus($file,false,$user);
   }
 
-  private function save() {
-    if(saveRewrite($this->destinationFile,$this->contentValue) === false)
+  private function save($dest,$content) {
+    if(saveRewrite($dest,$content) === false)
       throw new Exception("Unable to save changes, administration may be locked (update in progress)");
-    if(empty($this->errors)) $this->redir($this->destinationFile);
+    if(empty($this->errors)) $this->redir($this->defaultFile);
   }
 
   private function isHtmlPlus() {
