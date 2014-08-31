00a60c103d313e48a6ffa28bb063f68969a6a590
Jiri <j.pavelka@seznam.cz>
2014-08-31 15:50:45 +0200

    backup all usr/adm files

--- plugins/Backup/Backup.php
+++ plugins/Backup/Backup.php
@@ -1,48 +1,78 @@
 <?php
 
-#TODO: change implementation...
-
 class Backup extends Plugin implements SplObserver {
 
   const HASH_FILE_ALGO = 'crc32b';
-  const BACKUP_FILENAME_SEPARATOR = "~";
-  const BACKUP_DATE_FORMAT = "YmdH"; // eg YmdH for 2011091513
-  #const CORRUPTED_FILE_EXTENSION = ".corrupted";
 
   /**
-   * Get information about a file from file path including backup directory
-   * @param  string $filePath Path to a file
-   * @return Array (
-   *     [dirname] => ../cms
-   *     [basename] => Cms.xml
-   *     [extension] => xml
-   *     [filename] => Cms
-   *     [backupdirname] => bak
-   * )
+   * Observer update method
+   * @param  SplSubject $subject Subject with observer collection
+   * @return void
    */
-  private function getFileInfo($filePath) {
-    $fileInfo = pathinfo($filePath);
-    $backupDir = null;
-    if(strpos($filePath, ADMIN_FOLDER) === 0) {
-      $backupDir = str_replace(ADMIN_FOLDER,ADMIN_BACKUP,$fileInfo["dirname"]);
-    } elseif(strpos($filePath, USER_FOLDER) === 0) {
-      $backupDir = str_replace(USER_FOLDER,USER_BACKUP,$fileInfo["dirname"]);
-    }
-    $fileInfo["backupdirname"] = $backupDir;
-    return $fileInfo;
+  public function update(SplSubject $subject) {
+    if($subject->getStatus() != "process") return;
+    $this->subject = $subject;
+    $this->backupFilesDeep(ADMIN_FOLDER,ADMIN_BACKUP);
+    $this->backupFilesDeep(USER_FOLDER,USER_BACKUP);
   }
 
-  public function getNewestBackupFilePath($filePath) {
+  private function backupFilesDeep($dir,$backupDir) {
+    $cfg = $this->getDOMPlus();
+    $xpath = new DOMXPath($cfg);
+    $deny = array();
+    foreach($xpath->query("deny/ext") as $e) $deny[] = $e->nodeValue;
+    foreach(scandir($dir) as $file) {
+      if($file == "." || $file == "..") continue;
+      if(is_dir("$dir/$file")) {
+        $this->backupFilesDeep("$dir/$file","$backupDir/$file");
+        continue;
+      }
+      if(in_array(pathinfo($file,PATHINFO_EXTENSION), $deny)) continue;
+      $this->doBackup("$dir/$file",$backupDir ."/". $this->getBackupFileName("$dir/$file"));
+    }
+  }
+
+  private function getBackupFileName($filePath) {
+    $pi = pathinfo($filePath);
+    return sprintf("%s.%s",$pi["basename"],$this->getFileHash($filePath));
+  }
+
+  private function doBackup($src,$dest) {
+    if(file_exists($dest)) return;
+    $destDir = pathinfo($dest,PATHINFO_DIRNAME);
+    if(is_dir($destDir)) foreach(scandir($destDir) as $f) {
+      if(pathinfo($f,PATHINFO_FILENAME) != pathinfo($src,PATHINFO_BASENAME)) continue;
+      if(filectime($f) > time() - 60*60) return;
+    }
+    if(!is_dir($destDir) && !mkdir($destDir,0755,true))
+      throw new Exception("Unable to create backup directory '$destDir'");
+    if(!copy($src,$dest)) {
+      throw new Exception("Unable to create backup '$dest'");
+    }
+  }
+
+  private function getFileHash($filePath) {
+    return hash_file(self::HASH_FILE_ALGO,$filePath);
+  }
+
+  #UNUSED (restore)
+  public function restoreNewestBackup($filePath) {
+    $fileInfo = $this->getFileInfo($filePath);
+    $backupFileName = $this->getNewestBackupFileName($fileInfo["backupdirname"],$fileInfo["filename"]);
+    if(is_file($filePath) && !rename($filePath,$filePath . self::CORRUPTED_FILE_EXTENSION))
+      throw new Exception("Unable to rename corrupted file $filePath");
+    if(!rename($fileInfo["backupdirname"] ."/". $backupFileName,$filePath))
+      throw new Exception("Unable to restore newest backup $filePath");
+    $this->doBackup($filePath);
+  }
+
+  #UNUSED (restore)
+  private function getNewestBackupFilePath($filePath) {
     $fileInfo = $this->getFileInfo($filePath);
     return $fileInfo["backupdirname"] . "/" . $this->getNewestBackupFileName($filePath);
   }
 
-  /**
-   * Get newest backup file
-   * @param  string $filePath Origin file path
-   * @return string           Newest backup file name
-   * @throws Exception If no backup file found
-   */
+  #UNUSED (restore)
   private function getNewestBackupFileName($filePath) {
     $fileInfo = $this->getFileInfo($filePath);
     if(is_dir($fileInfo["backupdirname"])) {
@@ -55,62 +85,6 @@ class Backup extends Plugin implements SplObserver {
       }
     }
     throw new Exception("Unable to find backup of '$filePath'");
-  }
-
-  /**
-   * Observer update method
-   * @param  SplSubject $subject Subject with observer collection
-   * @return void
-   */
-  public function update(SplSubject $subject) {
-    if($subject->getStatus() != "preinit") return;
-    #todo: do backup!
-  }
-
-  /**
-   * Do backup of given file if not too young or the same
-   * @param  string $filePath Path to original file
-   * @return void
-   * @throws Exception        If unable to create backup file
-   */
-  public function doBackup($filePath) {
-    $fileInfo = $this->getFileInfo($filePath);
-    if(is_null($fileInfo["backupdirname"])) return;
-    $fileHash = hash_file(self::HASH_FILE_ALGO,$filePath);
-    try {
-      $newestBackupFile = $this->getNewestBackupFileName($filePath);
-      // check hash
-      if(strpos($newestBackupFile,$fileHash) !== false) return;
-      // check timestamp
-      if(strpos($newestBackupFile,date(self::BACKUP_DATE_FORMAT)) !== false) return;
-    } catch(Exception $e) {}
-    $s = self::BACKUP_FILENAME_SEPARATOR;
-    $backupFileName = sprintf("%s$s%s$s%s.%s",$fileInfo["filename"],
-      date(self::BACKUP_DATE_FORMAT),$fileHash,$fileInfo["extension"]);
-    $backupFilePath = $fileInfo["backupdirname"] ."/". $backupFileName;
-    if(is_file($backupFilePath)) return;
-    if(!is_dir($fileInfo["backupdirname"])) mkdir($fileInfo["backupdirname"],0755,true);
-    if(!copy($filePath,$backupFilePath)) {
-      throw new Exception("Unable to create backup $backupFilePath");
-    }
-  }
-
-  /**
-   * Restore newest backup of a given corrupted or missing file
-   * @param  string $filePath Filename to be restored
-   * @return void
-   * @throws Exception If unable to rename corrupted file
-   * @throws Exception If unable to find backup file
-   * @throws Exception If unable to restore newest backup
-   */
-  public function restoreNewestBackup($filePath) {
-    $fileInfo = $this->getFileInfo($filePath);
-    $backupFileName = $this->getNewestBackupFileName($fileInfo["backupdirname"],$fileInfo["filename"]);
-    if(is_file($filePath) && !rename($filePath,$filePath . self::CORRUPTED_FILE_EXTENSION))
-      throw new Exception("Unable to rename corrupted file $filePath");
-    if(!rename($fileInfo["backupdirname"] ."/". $backupFileName,$filePath))
-      throw new Exception("Unable to restore newest backup $filePath");
-    $this->doBackup($filePath);
   }
 
 }
