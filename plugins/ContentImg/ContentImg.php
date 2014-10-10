f463cafd24fdfcd34a3ee878bce20b2785710988
Jiri <j.pavelka@seznam.cz>
2014-10-10 12:44:49 +0200

    contentImg concept changed

--- plugins/ContentImg/ContentImg.php
+++ plugins/ContentImg/ContentImg.php
@@ -12,60 +12,60 @@ class ContentImg extends Plugin implements SplObserver, ContentStrategyInterface
   }
 
   public function getContent(HTMLPlus $content) {
-    foreach($this->getDOMPlus()->getElementsByTagName("var") as $var) {
-      if(!$var->hasAttribute("id")) throw new Exception ("Missing id attr in element var");
-      if(!$var->hasAttribute("pattern")) throw new Exception ("Missing pattern attr in element var");
-      $images = matchFiles($var->getAttribute("pattern"),THUMBS_FOLDER);
-      if(!count($images)) continue;
-      $id = $var->getAttribute("id");
+    $cfg = $this->getDomPlus();
+    $xpath = new DOMXPath($content);
+    foreach($xpath->query("//ul[@var and contains(@var,'ContentImg:')]") as $ul) {
       $dom = new DOMDocumentPlus();
-      $ul = $dom->createElement("ul");
-      $limit = 100;
-      if($var->hasAttribute("limit"))
-        $limit = (int) $var->getAttribute("limit");
-      $descFor = array();
-      $desc = array();
-      foreach($var->childNodes as $d) {
-        if(!$d->hasAttribute("for")) {
-          $desc[] = $d->nodeValue;
-          continue;
-        }
-        $descFor[THUMBS_FOLDER . "/" . $d->getAttribute("for")] = $d->nodeValue;
+      $list = $dom->createElement("ul");
+      foreach(explode(" ", $ul->getAttribute("var")) as $val) {
+        if(strpos($val,"ContentImg:") !== 0) continue;
+        $pattern = substr($val,11);
+        $this->getImages($list, $pattern, $cfg);
+        if($list->childNodes->length == 0) continue;
+        $content->insertVar($pattern,$list,"ContentImg");
       }
-      foreach($images as $i) {
-        if(--$limit < 0) break;
-        $imgInfo = getimagesize($i);
-        if(!$this->isSupportedImg($i,$imgInfo["mime"])) continue;
-        #todo: check dimensions
-
-        $title = pathinfo($i,PATHINFO_FILENAME);
-        if(array_key_exists($i, $descFor)) $title = $descFor[$i];
-        elseif(count($desc)) $title = array_shift($desc);
-
-        $li = $dom->createElement("li");
-        $ul->appendChild($li);
-        $obj = $dom->createElement("object",$title);
-        $obj->setAttribute("data",$i);
-        $obj->setAttribute("width",$imgInfo[0]);
-        $obj->setAttribute("height",$imgInfo[1]);
-        $obj->setAttribute("type",$imgInfo["mime"]);
-        $obj->setAttribute("title",$title);
-
-        $pic = PICTURES_FOLDER . "/" . pathinfo($i,PATHINFO_BASENAME);
-        if(file_exists($pic) && $this->isSupportedImg($pic)) {
-          $a = $dom->createElement("a");
-          $a->appendChild($obj);
-          $a->setAttribute("href",$pic);
-          $li->appendChild($a);
-          continue;
-        }
-        $li->appendChild($obj);
-      }
-      if(!$ul->childNodes->length)
-        $ul->appendChild($dom->createElement("li","Image(s) not found"));
-      $content->insertVar($id,$ul,"ContentImg");
     }
     return $content;
+  }
+
+  public function getImages(DOMElement $ul, $pattern, DOMDocumentPlus $cfg) {
+    $images = matchFiles($pattern,THUMBS_FOLDER);
+    if(!count($images)) return;
+    $desc = array();
+    foreach($cfg->getElementsByTagName("desc") as $d) {
+      if(!$d->hasAttribute("id")) continue;
+      $desc[THUMBS_FOLDER . "/" . $d->getAttribute("id")] = $d->nodeValue;
+    }
+    $limit = 100;
+    foreach($images as $i) {
+      if(--$limit < 0) break;
+      $imgInfo = getimagesize($i);
+      if(!$this->isSupportedImg($i,$imgInfo["mime"])) continue;
+      #todo: check dimensions
+
+      #todo: sophisticated default desc
+      $title = pathinfo($i,PATHINFO_FILENAME);
+      if(array_key_exists($i, $desc)) $title = $desc[$i];
+
+      $li = $ul->ownerDocument->createElement("li");
+      $ul->appendChild($li);
+      $obj = $ul->ownerDocument->createElement("object",$title);
+      $obj->setAttribute("data",$i);
+      $obj->setAttribute("width",$imgInfo[0]);
+      $obj->setAttribute("height",$imgInfo[1]);
+      $obj->setAttribute("type",$imgInfo["mime"]);
+      $obj->setAttribute("title",$title);
+
+      $pic = PICTURES_FOLDER . "/" . pathinfo($i,PATHINFO_BASENAME);
+      if(file_exists($pic) && $this->isSupportedImg($pic)) {
+        $a = $ul->ownerDocument->createElement("a");
+        $a->appendChild($obj);
+        $a->setAttribute("href",$pic);
+        $li->appendChild($a);
+        continue;
+      }
+      $li->appendChild($obj);
+    }
   }
 
   private function isSupportedImg($img,$mime=null) {
