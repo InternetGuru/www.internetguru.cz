bbf977845a07f639cf38d00e2a599c704c76a0d0
Jiri <j.pavelka@seznam.cz>
2014-10-23 14:05:25 +0200

    500 error page

--- index.php
+++ index.php
@@ -43,11 +43,6 @@ try {
     new Logger($e->getMessage(),"fatal");
   } catch (Exception $e) {};
 
-  #if(isAtLocalhost()) {
-  #  echo "Exception: ".$e->getMessage()." in ".$e->getFile()." on line ".$e->getLine();
-  #  exit;
-  #}
-
   http_response_code(500);
   #TODO: http://xkcd.com/1350/#p:10e7f9b6-b9b8-11e3-8003-002590d77bdd
   $dir = CMS_FOLDER."/lib/500";
@@ -56,18 +51,18 @@ try {
     "Tak s tímhle jsme nepočítali",
     );
   $m = array(
+    "Gratulujeme, objevili jste skrytou <del>chybu</del> komnatu.",
     "Náš ústav se vám jménem W3G co nejsrdečněji omlouvá za tuto politováníhodnou skutečnost, ke které dochází maximálně #ERROR#krát za 10 let."
     );
   $i = array();
   foreach(scandir($dir) as $img) {
     if(pathinfo("$dir/$img",PATHINFO_EXTENSION) == "png") $i[] = "$dir/$img";
   }
-  $s = file_get_contents(CMS_FOLDER."/".THEMES_FOLDER."/cms-default.css");
-  $s .= file_get_contents(CMS_FOLDER."/".THEMES_FOLDER."/simple.css");
-  $s .= "body{background:url('".$i[array_rand($i)]."') no-repeat;}";
   $html = file_get_contents(CMS_FOLDER."/$dir/500.cs.html");
-  $search = array('@HEADING@','@MESSAGE@','@ERROR@','@STYLE@');
-  $replace = array($h[array_rand($h)],$m[array_rand($m)],$e->getMessage(),$s);
+  $search = array('@HEADING@','@MESSAGE@','@ERROR@','@IMAGE@');
+  $ex = $e->getMessage();
+  if(isAtLocalhost()) $ex = "Exception: ".$e->getMessage()." in ".$e->getFile()." on line ".$e->getLine();
+  $replace = array($h[array_rand($h)],$m[array_rand($m)],$ex,$i[array_rand($i)]);
   echo str_replace($search,$replace,$html);
 
 }
