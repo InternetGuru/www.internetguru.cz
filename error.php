7f0bbedd8f6020a6c5e5cfa069db2b49daa733d9
Jiri Pavelka <j.pavelka@seznam.cz>
2014-10-30 22:20:10 +0100

    .error.php fixed paths and images

--- error.php
+++ error.php
@@ -1,6 +1,6 @@
 <?php
 
-$dir = CMS_FOLDER."/lib/error";
+$dir = "/lib/error";
 $h = array(
   "Něco je špatně",
   "Tak s tímhle jsme nepočítali",
@@ -12,10 +12,11 @@ $m = array(
   );
 $i = array();
 // http://xkcd.com/1350/#p:10e7f9b6-b9b8-11e3-8003-002590d77bdd
-foreach(scandir($dir) as $img) {
-  if(pathinfo("$dir/$img",PATHINFO_EXTENSION) == "png") $i[] = "$dir/$img";
+foreach(scandir(CMS_FOLDER ."/$dir") as $img) {
+  if(pathinfo(CMS_FOLDER ."/$dir/$img",PATHINFO_EXTENSION) != "png") continue;
+  $i[] = findFile("$dir/$img",false,false,true);
 }
-$html = file_get_contents(CMS_FOLDER."/$dir/error.cs.html");
+$html = file_get_contents(CMS_FOLDER ."$dir/error.cs.html");
 $search = array('@HEADING@','@MESSAGE@','@ERROR@','@IMAGE@','@ERRNR@','@ROOT@');
 $replace = array($h[array_rand($h)], $m[array_rand($m)], $message, $i[array_rand($i)], http_response_code(), getRoot());
 echo str_replace($search,$replace,$html);
