b8e146d6ffd0c844ce14d74b33781a2a8f9c118d
Jiri Pavelka <j.pavelka@seznam.cz>
2014-10-31 01:25:41 +0100

    FileHandler plugin created

--- cls/globals.php
+++ cls/globals.php
@@ -316,51 +316,6 @@ function getFileMime($file) {
   return $mime;
 }
 
-function handleFile() {
-  $requestUri = $_SERVER["REQUEST_URI"];
-  if(strpos($requestUri,"/") === 0) $requestUri = substr($requestUri,1); // remove trailing slash
-  $filePathPattern = "/^".preg_quote(getRoot(),"/")."(?:[a-zA-Z0-9_-]+\/)*[a-zA-Z0-9._-]+\.[a-z0-9]{2,4}$/";
-  if(!preg_match($filePathPattern,$requestUri,$m)) return;
-  $filePath = FILES_FOLDER . $m[0];
-  if(isAtLocalhost() || !is_file($filePath)) errorPage("File not found", 404);
-  $size = filesize($filePath);
-  $l = new Logger("File download '$filePath' ".fileSizeConvert($size),null,false);
-  $disallowedMime = array(
-    "application/x-msdownload" => null,
-    "application/x-msdos-program" => null,
-    "application/x-msdos-windows" => null,
-    "application/x-download" => null,
-    "application/bat" => null,
-    "application/x-bat" => null,
-    "application/com" => null,
-    "application/x-com" => null,
-    "application/exe" => null,
-    "application/x-exe" => null,
-    "application/x-winexe" => null,
-    "application/x-winhlp" => null,
-    "application/x-winhelp" => null,
-    "application/x-javascript" => null,
-    "application/hta" => null,
-    "application/x-ms-shortcut" => null,
-    "application/octet-stream" => null,
-    "vms/exe" => null,
-  );
-  $mime = getFileMime($filePath);
-  if(array_key_exists($mime,$disallowedMime)) errorPage("Unsupported Media Type", 415);
-  header("Content-Type: $mime");
-  header("Content-Length: $size");
-  set_time_limit(0);
-  $file = @fopen($filePath,"rb");
-  if($file === false) throw new Exception("Unable to read from file '$filePath'");
-  while(!feof($file)) {
-    print(fread($file, 1024*8));
-    ob_flush();
-    flush();
-  }
-  $l->finished();
-  die();
-}
-
 function fileSizeConvert($b) {
     if(!is_numeric($b)) return $b;
     $i = 0;
