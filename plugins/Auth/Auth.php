1956d9415b50adc2ff9c2168689f39e04ca51955
jiripavelka <j.pavelka@seznam.cz>
2014-11-17 17:15:45 +0100

    Added new plugin Auth

---
+++ plugins/Auth/Auth.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class Auth extends Plugin implements SplObserver {
+
+  public function __construct(SplSubject $s) {
+    parent::__construct($s);
+    $s->setPriority($this,1);
+  }
+
+  public function update(SplSubject $subject) {
+    if($subject->getStatus() != STATUS_PREINIT) return;
+    $this->handleRequest();
+  }
+
+  private function handleRequest() {
+    $cfg = $this->getDOMPlus();
+    $url = getCurLink(true);
+    $access = true;
+    foreach($cfg->getElementsByTagName('url') as $e) {
+      if(strpos($url, $e->nodeValue) === false) continue;
+      if($e->hasAttribute("access") && $e->getAttribute("access") == "allow") $access = true;
+      else $access = false;
+    }
+    if($access) return;
+    if(isset($_SERVER['REMOTE_USER']) && in_array($_SERVER['REMOTE_USER'], array(USER_ID,"admin"))) return;
+    new ErrorPage("Authentication required", 403);
+  }
+
+}
+
+?>
