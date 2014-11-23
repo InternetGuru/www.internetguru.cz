9275e203fad997c0128339aecf4396c6f1bd69a9
jiripavelka <j.pavelka@seznam.cz>
2014-11-23 10:49:35 +0100

    Auth: variable logged_user fixed

--- plugins/Auth/Auth.php
+++ plugins/Auth/Auth.php
@@ -2,16 +2,19 @@
 
 
 class Auth extends Plugin implements SplObserver {
+  private $loggedUser;
 
   public function __construct(SplSubject $s) {
     parent::__construct($s);
-    $s->setPriority($this,1);
+    $s->setPriority($this,0);
   }
 
   public function update(SplSubject $subject) {
     if(isAtLocalhost()) return;
-    if($subject->getStatus() != STATUS_PREINIT) return;
-    $this->handleRequest();
+    if($subject->getStatus() == STATUS_PREINIT) $this->handleRequest();
+    if($subject->getStatus() != STATUS_INIT) return;
+    global $cms;
+    $cms->insertVar("logged_user", $this->loggedUser);
   }
 
   private function handleRequest() {
@@ -23,14 +26,12 @@ class Auth extends Plugin implements SplObserver {
       if($e->hasAttribute("access") && $e->getAttribute("access") == "allow") $access = true;
       else $access = false;
     }
-    $loggedUser = null;
+    $this->loggedUser = null;
     if(isset($_SERVER['REMOTE_USER'])
       && in_array($_SERVER['REMOTE_USER'], array(USER_ID,"admin"))) {
-      $loggedUser = $_SERVER['REMOTE_USER'];
+      $this->loggedUser = $_SERVER['REMOTE_USER'];
     }
-    global $cms;
-    $cms->insertVar("logged_user", $loggedUser);
-    if($access || !is_null($loggedUser)) return;
+    if($access || !is_null($this->loggedUser)) return;
     new ErrorPage(_("Authentication required"), 403);
   }
 
