bbc1669890950592d37a2740618ed860ad5a59b1
jiripavelka <j.pavelka@seznam.cz>
2015-01-08 13:16:42 +0100

    Cms output: Cache-control public, Etag

--- core/init_cms.php
+++ core/init_cms.php
@@ -2,6 +2,7 @@
 try {
 
   $start_time = microtime(true);
+  session_cache_limiter("public");
   session_start();
   require_once('global_func.php');
   require_once('global_const.php');
@@ -34,7 +35,22 @@ try {
   duplicateDir(USER_FOLDER);
   duplicateDir(ADMIN_FOLDER);
   if(defined("SUBDOM_FOLDER")) duplicateDir(SUBDOM_FOLDER, false);
-  echo Cms::getOutput();
+
+  $out = Cms::getOutput();
+  //get a unique hash of this file (etag)
+  $etagFile = hash("md5", $out);
+  //get the HTTP_IF_NONE_MATCH header if set (etag: unique file hash)
+  $etagHeader=(isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
+  //set etag-header
+  header("Etag: $etagFile");
+  //check if page has changed. If not, send 304 and exit
+  if ($etagHeader == $etagFile) {
+    header("HTTP/1.1 304 Not Modified");
+    new Logger(_("Page not modified"), Logger::LOGGER_INFO);
+  } else {
+    echo $out;
+  }
+
   new Logger(sprintf(_("IGCMS successfully finished"), CMS_RELEASE), Logger::LOGGER_INFO, $start_time);
 
 } catch(Exception $e) {
