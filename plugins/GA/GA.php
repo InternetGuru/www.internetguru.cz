10d5565e4e9acaf32751ddad9bc1e62e2ff4f0d6
jiripavelka <j.pavelka@seznam.cz>
2015-03-05 08:24:25 +0100

    GA: messages improved

--- plugins/GA/GA.php
+++ plugins/GA/GA.php
@@ -5,14 +5,11 @@ class GA extends Plugin implements SplObserver {
   public function update(SplSubject $subject) {
     if($subject->getStatus() != STATUS_PROCESS) return;
     if($this->detachIfNotAttached("Xhtml11")) return;
-    if(Cms::isSuperUser()) {
-      $subject->detach($this);
-      return;
-    }
     $this->init();
   }
 
   private function init() {
+    // setup ga_id
     $ga_id = $this->getDOMPlus()->getElementById("ga_id");
     if(!strlen($ga_id->nodeValue)) {
       $ga_id = $this->getDOMPlus()->getElementById(DOMAIN);
@@ -21,21 +18,23 @@ class GA extends Plugin implements SplObserver {
       $ga_id = $this->getDOMPlus()->getElementById("other");
     }
     $ga_id = $ga_id->nodeValue;
-    if(!$this->isValidId($ga_id)) {
-      Cms::getOutputStrategy()->addJs("// ".sprintf(_("Invalid ga_id format '%s'"), $ga_id), 1, "body");
-      $this->subject->detach($this);
+    // ga_id validation
+    if(!preg_match("/^UA-\d+-\d+$/", $ga_id)) {
+      new Logger(sprintf(_("Invalid ga_id format '%s'"), $ga_id), Logger::LOGGER_WARNING);
       return;
     }
+    // disable if superadmin
+    if(Cms::isSuperUser()) {
+      Cms::getOutputStrategy()->addJs("// ".sprintf(_("GA (%s) disabled if administrator logged"), $ga_id), 1, "body");
+      return;
+    }
+    // add js into html
     Cms::getOutputStrategy()->addJs("var ga_id = '$ga_id';", 1, "body");
     foreach($this->getDOMPlus()->getElementsByTagName("jsFile") as $jsFile) {
       $user = !$jsFile->hasAttribute("readonly");
       $f = $this->pluginDir."/".$jsFile->nodeValue;
       Cms::getOutputStrategy()->addJsFile($f, 1, "body", $user);
     }
-  }
-
-  private function isValidId($id) {
-    return preg_match("/^UA-\d+-\d+$/", $id);
   }
 
 }
