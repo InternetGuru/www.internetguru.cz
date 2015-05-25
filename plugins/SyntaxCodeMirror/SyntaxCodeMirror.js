ccb64bbe55e809fb5c31ec803737c89b91bd8b2d
Pavel Petrzela <pavel@petrzela.eu>
2015-05-25 13:54:43 +0200

    SyntaxCodeMirror: added js on/off codemirror

--- plugins/SyntaxCodeMirror/SyntaxCodeMirror.js
+++ plugins/SyntaxCodeMirror/SyntaxCodeMirror.js
@@ -2,8 +2,37 @@
 
   // CodeMirror Configuration
   // http://codemirror.net/doc/manual.html
+  var cm = false
   var TextArea = document.getElementsByTagName("textarea")[0];
-  var cm = CodeMirror.fromTextArea(TextArea,{
+  var visible = true;
+  var on = "Vypnout CodeMirror";
+  var off = "Zapnout CodeMirror";
+
+  init(TextArea);
+
+  var ul = document.createElement("ul");
+  var li = document.createElement("li");
+  ul.appendChild(li);
+  var toggleButton = document.createElement("button");
+  li.appendChild(toggleButton);
+  toggleButton.type = "button";
+  toggleButton.innerText = on;
+  TextArea.parentNode.insertBefore(ul, TextArea);
+  toggleButton.onclick = function() {
+    if(visible) {
+      cm.toTextArea();
+      toggleButton.innerText = off;
+    }
+    else {
+      init(cm.getTextArea());
+      toggleButton.innerText = on;
+    }
+    visible = !visible;
+  }
+
+
+  function init(textarea) {
+    cm = CodeMirror.fromTextArea(TextArea, {
       tabMode: "default",
       keyMap:"sublime",
       theme:"tomorrow-night-eighties",
@@ -25,13 +54,13 @@
         "End": "goLineRight",
         "Home": "goLineLeft"
       }
-  });
-
-  cm.on("change",function(cm,change) {
-    if(!Editable) return;
-    var form = Editable.getParentForm(TextArea);
-    if(!form || !form.classList.contains(Editable.getEditableClass())) return;
-    Editable.setModified();
-  });
+    });
+    cm.on("change",function(cm,change) {
+      if(!Editable) return;
+      var form = Editable.getParentForm(TextArea);
+      if(!form || !form.classList.contains(Editable.getEditableClass())) return;
+      Editable.setModified();
+    });
+  }
 
 })(window);
