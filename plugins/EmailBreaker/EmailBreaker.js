9c259102ceb1bd016b859ab85e2d1e4abe30c262
jiripavelka <j.pavelka@seznam.cz>
2015-06-19 14:13:12 +0200

    minor fixes

--- plugins/EmailBreaker/EmailBreaker.js
+++ plugins/EmailBreaker/EmailBreaker.js
@@ -21,7 +21,7 @@ var a = 1;
         Config.at = preg_quote(Config.at);
         Config.dot = preg_quote(Config.dot);
         var emailPatt = new RegExp("\\b([_a-zA-Z0-9.-]+)"+Config.at+"([_a-zA-Z0-9.-]+)"+Config.dot+"([a-zA-Z]{2,})\\b", "g");
-        document.body.innerHTML = document.body.innerHTML.replace(emailPatt, "<a href='mailto:$1@$2.$3'>$1@$2.$3</a>");
+        document.body.innerHTML = document.body.innerHTML.replace(emailPatt, "<"+"a href='mailto:$1@$2.$3'>$1@$2.$3<"+"/a>");
       },
       preg_quote = function (str, delimiter) {
         return (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
