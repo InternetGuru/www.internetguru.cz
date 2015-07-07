f90a8076c07c1a0923af97e32a3a1a63dcd13e55
Pavel Petrzela <pavel@petrzela.eu>
2015-07-07 18:20:43 +0200

    HtmlOutput: added browserdetect.js and iefixes.js

---
+++ themes/iefixes.js
@@ -0,0 +1,32 @@
+(function(){
+
+  function appendStyle(css) {
+    var style = document.getElementsByTagName('style')[0];
+    if(style == undefined) {
+      var head = document.head || document.getElementsByTagName('head')[0];
+      style = document.createElement('style');
+      style.type = 'text/css';
+      head.appendChild(style);
+    }
+    style.appendChild(document.createTextNode(css));
+  }
+
+  function hasObjectChild(e) {
+    for(var i = 0; i < e.childNodes.length; i++) {
+      if(e.childNodes[i].nodeName.toLowerCase() == "object") return true;
+      if(e.childNodes[i].hasChildNodes()) return hasObjectChild(e.childNodes[i]);
+    }
+    return false;
+  }
+
+  function fixObjectInHref() {
+    var anchors = document.getElementsByTagName("a");
+    for(var i = 0; i < anchors.length; i++) {
+      if(hasObjectChild(anchors[i])) anchors[i].className += "obj";
+    }
+    var css ='a.obj{position:relative;display:inline-block} a.obj:after{content:\'\';position:absolute;left:0;top:0;height:100%;width:100%;background:url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7")}';
+    appendStyle(css);
+  }
+
+  fixObjectInHref();
+})();
