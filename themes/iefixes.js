a0ffca79be4a6b666387dd95351b6b8028858747
Pavel Petrzela <pavel@petrzela.eu>
2015-07-08 10:00:06 +0200

    VERSION updated

--- themes/iefixes.js
+++ themes/iefixes.js
@@ -24,7 +24,7 @@
     for(var i = 0; i < anchors.length; i++) {
       if(hasObjectChild(anchors[i])) anchors[i].className += "obj";
     }
-    var css ='a.obj{position:relative;display:inline-block} a.obj:after{content:\'\';position:absolute;left:0;top:0;height:100%;width:100%;background:url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7")}';
+    var css ='html[data-useragent*=\'Trident\'] a.obj, html[data-useragent*=\'MSIE\'] a.obj{position:relative;display:inline-block} html[data-useragent*=\'Trident\'] a.obj:after, html[data-useragent*=\'MSIE\'] a.obj:after{content:\'\';position:absolute;left:0;top:0;height:100%;width:100%;background:url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7")}';
     appendStyle(css);
   }
 
