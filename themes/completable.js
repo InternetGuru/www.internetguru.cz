fa6e6ea57a4bda7416c3a62dd1fb02a82688b052
jiripavelka <j.pavelka@seznam.cz>
2015-11-29 21:33:12 +0100

    completable: fixed regex

--- themes/completable.js
+++ themes/completable.js
@@ -177,7 +177,7 @@
     filter = function(arr, value) {
       var fs = [];
       var qvalue = IGCMS.preg_quote(value).replace(/\\\*/g, "[^/ ]*");
-      var pattern = new RegExp("(^|[^a-z])("+qvalue+")", "gi");
+      var pattern = new RegExp("(^|[^a-z0-9])("+qvalue+")", "gi");
       for(var i = 0; i < arr.length; i++) {
         var r = doFilter(arr[i], value, pattern);
         if(typeof r != "undefined") {
