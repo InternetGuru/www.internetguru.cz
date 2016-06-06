abf54168b5aa27b838b3943a71feaf17766384ce
jiripavelka <j.pavelka@seznam.cz>
2016-06-06 21:11:07 +0200

    fix SyntaxCodeMirror on change event

--- plugins/SyntaxCodeMirror/SyntaxCodeMirror.js
+++ plugins/SyntaxCodeMirror/SyntaxCodeMirror.js
@@ -3,7 +3,7 @@ if(typeof IGCMS === "undefined") throw "IGCMS is not defined";
 
 CodeMirrorInstance.on("change",function(cm,change) {
   if(!IGCMS.Editable) return;
-  var form = IGCMS.Editable.getParentForm(textarea);
+  var form = IGCMS.Editable.getParentForm(CodeMirrorInstance.getTextArea());
   if(!form || !form.classList.contains(IGCMS.Editable.getEditableClass())) return;
   IGCMS.Editable.setModified();
 });
