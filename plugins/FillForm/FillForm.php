180cd57eed01f161dab7784517c33ddce3cf3251
jiripavelka <j.pavelka@seznam.cz>
2016-09-24 15:43:19 +0200

    fillform fix uncheck checkbox that is check by default

--- plugins/FillForm/FillForm.php
+++ plugins/FillForm/FillForm.php
@@ -12,33 +12,60 @@ use SplSubject;
 
 class FillForm extends Plugin implements SplObserver, ModifyContentStrategyInterface {
 
+  private $prefix;
+
   public function __construct(SplSubject $s) {
     parent::__construct($s);
     $s->setPriority($this, 100);
+    $this->prefix = strtolower($this->className)."-";
   }
 
   public function update(SplSubject $subject) {
     if($subject->getStatus() != STATUS_INIT) return;
     foreach(Cms::getAllVariables() as $varId => $formDoc) {
       if(strpos($varId, "contactform-") !== 0) continue;
-      $this->fillForm($formDoc->documentElement->firstChild);
+      $this->proceedForm($formDoc->documentElement->firstChild);
     }
   }
 
   public function modifyContent(HTMLPlus $content) {
     foreach($content->getElementsByTagName("form") as $form) {
-      $this->fillForm($form);
+      $this->proceedForm($form);
     }
   }
 
-  private function fillForm(DOMElementPlus $form) {
+  private function proceedForm(DOMElementPlus $form) {
     if(!$form->hasClass("fillable")) return;
+    $this->fixForm($form);
+    $this->fillForm($form);
+  }
+
+  private function fixForm(DOMElementPlus $form) {
+    $div = $form->ownerDocument->createElement("div");
+    foreach($form->getElementsByTagName("input") as $input) {
+      if($input->getAttribute("type") != "checkbox") continue;
+      $hidden = $form->ownerDocument->createElement("input");
+      $hidden->setAttribute("type", "hidden");
+      $hidden->setAttribute("value", "1");
+      $hidden->setAttribute("name", $this->prefix.$input->getAttribute("name"));
+      $div->appendChild($hidden);
+    }
+    $form->appendChild($div);
+  }
+
+  private function fillForm(DOMElementPlus $form) {
     #$post = strtolower($form->getAttribute("method")) == "post";
     foreach($form->getElementsByTagName("input") as $input) {
-      $value = $this->getRequestData($input->getAttribute("name"));
+      $name = $input->getAttribute("name");
+      $value = $this->getRequestData($name);
       #var_dump($input->getAttribute("name"));
       #var_dump($value);
-      if(is_null($value)) continue;
+      if(is_null($value)) {
+        if(!is_null($this->getRequestData($this->prefix.$name))) {
+          $input->removeAttribute("checked");
+        }
+        continue;
+      }
       switch($input->getAttribute("type")) {
         case "text":
         case "email":
@@ -52,8 +79,6 @@ class FillForm extends Plugin implements SplObserver, ModifyContentStrategyInter
         if($value == $input->getAttribute("value")
           || (is_array($value) && in_array($input->getAttribute("value"), $value))) {
           $input->setAttribute("checked", "checked");
-        } else {
-          $input->removeAttribute("checked");
         }
       }
     }
