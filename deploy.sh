3fc859a2433e3ba0e967a839267143e9747e660e
petrzpav <pavel@petrzela.eu>
2017-01-05 14:12:12 +0100

    do not deploy .gitignore

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 TO_UPLOAD=()
 
 for file in ${CHANGED_FILES[@]}; do
-  [[ "$file" == ".travis.yml" || "$file" == "deploy.sh" ]] && continue
+  [[ "$file" == ".travis.yml" || "$file" == "deploy.sh" || "$file" == ".gitignore" ]] && continue
   curl -k --ftp-ssl --ftp-create-dirs -T "$file" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/"$(dirname "$file")/"
 done
 
