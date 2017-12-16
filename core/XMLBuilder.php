eacc5a318a99b4a0cb336df59408b9c5a4b5a837
Jiri Pavelka <jiri.pavelka@internetguru.cz>
2017-12-16 16:37:31 -0500

    clean xmlbuilder

--- core/XMLBuilder.php
+++ core/XMLBuilder.php
@@ -12,12 +12,13 @@ class XMLBuilder extends DOMBuilder {
   /**
    * @param string $fileName
    * @return DOMDocumentPlus
+   * @throws Exception
    */
   public static function load ($fileName) {
     $doc = new DOMDocumentPlus();
-    $fp = findFile($fileName);
-    $doc->load($fp);
-    self::setNewestFileMtime(filemtime($fp));
+    $filePath = findFile($fileName);
+    $doc->load($filePath);
+    self::setNewestFileMtime(filemtime($filePath));
     return $doc;
   }
 
@@ -25,19 +26,20 @@ class XMLBuilder extends DOMBuilder {
    * @param string $fileName
    * @param bool $user
    * @return DOMDocumentPlus
+   * @throws Exception
    */
   public static function build ($fileName, $user = true) {
     $doc = new DOMDocumentPlus();
-    $fp = CMS_FOLDER."/$fileName";
-    $doc->load($fp);
-    self::setNewestFileMtime(filemtime($fp));
+    $filePath = CMS_FOLDER."/$fileName";
+    $doc->load($filePath);
+    self::setNewestFileMtime(filemtime($filePath));
 
-    $fp = ADMIN_FOLDER."/$fileName";
+    $filePath = ADMIN_FOLDER."/$fileName";
     try {
       $adminDoc = new DOMDocumentPlus();
-      $adminDoc->load($fp);
+      $adminDoc->load($filePath);
       self::updateDOM($doc, $adminDoc);
-      self::setNewestFileMtime(filemtime($fp));
+      self::setNewestFileMtime(filemtime($filePath));
     } catch (NoFileException $exc) {
       // skip
     } catch (Exception $exc) {
@@ -48,12 +50,12 @@ class XMLBuilder extends DOMBuilder {
       return $doc;
     }
 
-    $fp = USER_FOLDER."/$fileName";
+    $filePath = USER_FOLDER."/$fileName";
     try {
       $userDoc = new DOMDocumentPlus();
-      $userDoc->load($fp);
+      $userDoc->load($filePath);
       self::updateDOM($doc, $userDoc);
-      self::setNewestFileMtime(filemtime($fp));
+      self::setNewestFileMtime(filemtime($filePath));
     } catch (NoFileException $exc) {
       // skip
     } catch (Exception $exc) {
@@ -70,30 +72,30 @@ class XMLBuilder extends DOMBuilder {
    */
   private static function updateDOM (DOMDocumentPlus $doc, DOMDocumentPlus $newDoc) {
     $docId = null;
-    foreach ($newDoc->documentElement->childElementsArray as $n) {
-      if (!$n->hasAttribute("id")) {
-        self::appendChildFormat($doc->documentElement, $n);
+    foreach ($newDoc->documentElement->childElementsArray as $child) {
+      if (!$child->hasAttribute("id")) {
+        self::appendChildFormat($doc->documentElement, $child);
         continue;
       }
       if (is_null($docId)) {
         $docId = self::getIds($doc);
       }
-      $curId = $n->getAttribute("id");
+      $curId = $child->getAttribute("id");
       if (!array_key_exists($curId, $docId)) {
-        self::appendChildFormat($doc->documentElement, $n);
+        self::appendChildFormat($doc->documentElement, $child);
         continue;
       }
-      if ($docId[$curId]->nodeName != $n->nodeName) {
+      if ($docId[$curId]->nodeName != $child->nodeName) {
         throw new Exception(sprintf(_("Element id '%s' names differ"), $curId));
       }
       if ($docId[$curId]->hasAttribute("readonly")) {
         throw new Exception(sprintf(_("Element id '%s' is readonly"), $curId));
       }
       $pattern = $docId[$curId]->getAttribute("pattern");
-      if (strlen($pattern) && !preg_match("/^$pattern$/", $n->nodeValue)) {
+      if (strlen($pattern) && !preg_match("/^$pattern$/", $child->nodeValue)) {
         throw new Exception(sprintf(_("Element id '%s' pattern mismatch"), $curId));
       }
-      $doc->documentElement->replaceChild($doc->importNode($n, true), $docId[$curId]);
+      $doc->documentElement->replaceChild($doc->importNode($child, true), $docId[$curId]);
     }
   }
 
@@ -113,11 +115,11 @@ class XMLBuilder extends DOMBuilder {
    */
   private static function getIds (DOMDocumentPlus $doc) {
     $ids = [];
-    foreach ($doc->documentElement->childElementsArray as $n) {
-      if (!$n->hasAttribute("id")) {
+    foreach ($doc->documentElement->childElementsArray as $child) {
+      if (!$child->hasAttribute("id")) {
         continue;
       }
-      $ids[$n->getAttribute("id")] = $n;
+      $ids[$child->getAttribute("id")] = $child;
     }
     return $ids;
   }
