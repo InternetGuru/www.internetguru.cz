c605302ffc6d8d4dc91e830a89532af12d0dc1d2
Pavel Petržela <petrzpav@fit.cvut.cz>
2018-01-30 13:15:41 +0100

    optimize iefixes.js

--- themes/iefixes.js
+++ themes/iefixes.js
@@ -1,12 +1,6 @@
 (function(){
 
-  function appendStyle(css) {
-    var elem=document.createElement('style');
-    elem.setAttribute('type', 'text/css');
-    if(elem.styleSheet && !elem.sheet)elem.styleSheet.cssText=css;
-    else elem.appendChild(document.createTextNode(css));
-    document.getElementsByTagName('head')[0].appendChild(elem);
-  }
+  if(typeof IGCMS === "undefined") throw "IGCMS is not defined";
 
   function hasObjectChild(e) {
     for(var i = 0; i < e.childNodes.length; i++) {
@@ -22,7 +16,7 @@
       if(hasObjectChild(anchors[i])) anchors[i].className += " obj";
     }
     var css ='html[data-useragent*=\'Trident\'] a.obj, html[data-useragent*=\'MSIE\'] a.obj{position:relative;display:inline-block} html[data-useragent*=\'Trident\'] a.obj:after, html[data-useragent*=\'MSIE\'] a.obj:after{content:\'\';position:absolute;left:0;top:0;height:100%;width:100%;background:url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7")}';
-    appendStyle(css);
+    IGCMS.appendStyle(css);
   }
 
   fixObjectInHref();
